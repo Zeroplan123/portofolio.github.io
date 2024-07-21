497c158747566e7405e109bbb28368aad8a04e66
fahri RG <148167279+Zeroplan123@users.noreply.github.com>
2024-07-21 09:58:03 +0700

    Add files via upload

--- portofolio.js
+++ portofolio.js
@@ -1,11 +1,17 @@
-function showbar(){
-    let sidebar = document.querySelector(".menu-sidebar")
-    sidebar.style.display = "flex"
-    
+function showbar() {
+    let sidebar = document.querySelector(".menu-sidebar");
+    sidebar.style.display = "flex";
+    setTimeout(() => {
+        sidebar.style.transform = "translateX(0)";
+        sidebar.style.opacity = "1";
+    }, 10); // small delay to ensure the display property is set before transition
 }
-function hidebar(){
-    let hide = document.querySelector(".menu-sidebar")
-    hide.style.display = "none"
+
+function hidebar() {
+    let hide = document.querySelector(".menu-sidebar");
+    hide.style.transform = "translateX(100%)";
+    hide.style.opacity = "0";
+    setTimeout(() => hide.style.display = "none", 300);
 }
 
 var typed = new Typed(".auto-text", {
